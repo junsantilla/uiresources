7e92459e5a908f1b401df018cbc75b73a76f82b3
junsantilla <junsantilla@live.com>
2022-11-23 14:11:37 +0800

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { FaBug, FaHeartbeat } from "react-icons/fa";
+import { BiGitPullRequest } from "react-icons/bi";
+
+function Footer() {
+	return (
+		<footer className=" bg-base-200 p-8 py-10 ">
+			<div className="flex align-middle mb-1">
+				<FaBug className="inline m-1 mr-2 text-md" />
+				<p>
+					Do you see a bug?{" "}
+					<a
+						href="https://github.com/junsantilla/uiResources.io/issues"
+						className="underline"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Open an issue on GitHub
+					</a>
+				</p>
+			</div>
+
+			<div className="flex align-middle mb-1">
+				<BiGitPullRequest className="inline m-1 mr-2 text-md" />
+				<p>
+					Do you want to contribute?{" "}
+					<a
+						href="https://github.com/junsantilla/uiResources.io/pulls"
+						className="underline"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Pull request on Github
+					</a>
+				</p>
+			</div>
+			<div className="flex align-middle mb-1">
+				<FaHeartbeat className="inline m-1 mr-2 text-md" />
+				<p>
+					Do you like uiResources.io?{" "}
+					<a
+						href="https://twitter.com/intent/tweet?text=A%20huge%20collection%20of%20UI%20resources%20https://github.com/junsantilla/uiResources.io"
+						className="underline"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Tweet about it!
+					</a>
+				</p>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -3,6 +3,7 @@ import CollectionsData from "../data/CollectionsData";
 import { useLocation } from "react-router-dom";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import Topbar from "../components/Topbar";
+import Footer from "../components/Footer";
 
 function Collections({ title }) {
 	const [collections] = useState(CollectionsData);
@@ -13,49 +14,48 @@ function Collections({ title }) {
 	});
 
 	return (
-		<>
-			<div className="w-4/5 overflow-auto fixed right-0 h-screen">
-				<Topbar title={title} />
-				<div className="p-8">
-					<table className="table-auto w-full">
-						<thead className="text-left bg-base-200">
-							<tr className=" p-10">
-								<th className=" p-5">Name</th>
-								<th>Description</th>
-								<th></th>
+		<div className="w-4/5 overflow-auto fixed right-0 h-screen">
+			<Topbar title={title} />
+			<div className="p-8">
+				<table className="table-auto w-full">
+					<thead className="text-left bg-base-200">
+						<tr className=" p-10">
+							<th className=" p-5">Name</th>
+							<th>Description</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						{filtered.map((item, i) => (
+							<tr
+								className="border-b border-base-200 hover:bg-base-200 "
+								key={i}
+							>
+								<td className="font-bold p-2 px-5 pr-14">
+									{item.website}
+								</td>
+								<td>{item.description}</td>
+								<td className="flex justify-end p-3">
+									<a
+										href={item.url}
+										target="_blank "
+										className="w-max"
+									>
+										<button className="btn border-0 px-6  capitalize transition-none hover:text-primary-content hover:bg-primary hover:border-0">
+											<span className="mr-2">
+												<FaExternalLinkAlt />
+											</span>
+											Visit
+										</button>
+									</a>
+								</td>
 							</tr>
-						</thead>
-						<tbody>
-							{filtered.map((item, i) => (
-								<tr
-									className="border-b border-base-200 hover:bg-base-200 "
-									key={i}
-								>
-									<td className="font-bold p-2 px-5 pr-14">
-										{item.website}
-									</td>
-									<td>{item.description}</td>
-									<td className="flex justify-end p-3">
-										<a
-											href={item.url}
-											target="_blank "
-											className="w-max"
-										>
-											<button className="btn border-0 px-6  capitalize transition-none hover:text-primary-content hover:bg-primary hover:border-0">
-												<span className="mr-2">
-													<FaExternalLinkAlt />
-												</span>
-												Visit
-											</button>
-										</a>
-									</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
-				</div>
+						))}
+					</tbody>
+				</table>
 			</div>
-		</>
+			<Footer />
+		</div>
 	);
 }
 
