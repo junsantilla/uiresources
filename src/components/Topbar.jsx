6f34d03c63e6e6ee7fbb41b385093b00ff6d1d1b
junsantilla <junsantilla@live.com>
2022-12-09 00:06:34 +0800

    Add repo stars

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { FaGithub } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { FaGithub, FaStar } from "react-icons/fa";
 import { themeChange } from "theme-change";
 
 function Topbar({ title, drawerButton }) {
@@ -7,9 +7,29 @@ function Topbar({ title, drawerButton }) {
 		themeChange(false);
 	}, []);
 
+	const [stars, setStars] = useState([]);
+
+	const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+	const fetchRepo = async () => {
+		const response = await fetch(
+			`https://api.github.com/repos/junsantilla/uiresources.io`,
+			{
+				headers: {
+					Authorization: `token ${GITHUB_TOKEN}`,
+				},
+			}
+		);
+
+		const data = await response.json();
+		setStars(data.stargazers_count);
+	};
+
+	fetchRepo();
+
 	return (
-		<div className="flex w-full flex-col lg:flex-row justify-between items-center bg-base-200 p-8 py-5">
-			<h1 className="font-bold text-2xl lg:grow">{title}</h1>
+		<div className="flex w-full flex-col lg:flex-row justify-between md:items-center bg-base-200 p-8 py-5">
+			<h1 className="font-bold text-xl md:text-2xl lg:grow">{title}</h1>
 
 			<div className="flex items-center mt-4 lg:mt-0">
 				{drawerButton}
@@ -36,6 +56,16 @@ function Topbar({ title, drawerButton }) {
 					rel="noreferrer"
 				>
 					<FaGithub className="text-5xl hover:text-primary" />
+				</a>
+				<a
+					href="https://github.com/junsantilla/uiresources.io/stargazers"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<button className="btn btn-primary ml-3">
+						<FaStar className="mr-2" />
+						{stars}
+					</button>
 				</a>
 			</div>
 		</div>
